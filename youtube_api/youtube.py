3211b91da2bc5eec414de71e4b93e0b0ff725fc0
Ashis-debug <85397768+Ashis-debug@users.noreply.github.com>
2024-07-25 21:11:46 +0530

    Update youtube.py
    Added functionalities and fixed some bugs

--- youtube_api/youtube.py
+++ youtube_api/youtube.py
@@ -1,6 +1,6 @@
 # youtube.py
-
 import re
+import requests
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api._errors import (
     TranscriptsDisabled, NoTranscriptFound, VideoUnavailable
@@ -22,6 +22,49 @@ def get_youtube_transcript(video_id):
     """
     try:
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        message = " ".join([text['text'] for text in transcript])
+        return message
+    except VideoUnavailable:
+        return "The video is unavailable."
+    except TranscriptsDisabled:
+        return "Transcripts are disabled for this video."
+    except NoTranscriptFound:
+        return "No transcript is found for the given video."
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+
+def get_video_detail(videoID):
+    # access the API
+    url = "https://youtube-media-downloader.p.rapidapi.com/v2/video/details?"
+    headers = {
+        'x-rapidapi-key': "ce8371e650mshe45717dee95c8a8p1a0b57jsn195433104778",
+        'x-rapidapi-host': "youtube-media-downloader.p.rapidapi.com"
+    }
+    # send a get request to the API
+    querystring = {"videoId": videoID}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    # conver the response to json format
+    json_response = response.json()
+    # obtain the subtitle url (in XML format)
+    subtitleURL = json_response['subtitles']['items'][0]['url']
+
+    return subtitleURL
+
+
+def get_subtitle_text(subtitleUrl):
+    # access the API
+    url = "https://youtube-media-downloader.p.rapidapi.com//v2/video/subtitles?format=json&targetLang=en"
+    headers = {
+        'x-rapidapi-key': "ce8371e650mshe45717dee95c8a8p1a0b57jsn195433104778",
+        'x-rapidapi-host': "youtube-media-downloader.p.rapidapi.com"
+    }
+    # send a get subtitle text request to the API
+    querystring = {"subtitleUrl": subtitleUrl}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    # return the text response
+    try:
+        transcript = response.json()
         message = " ".join([text['text'] for text in transcript])
         return message
     except VideoUnavailable:
